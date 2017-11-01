e01b3070c354a67a25eb1f7020a543cda8a50116
Theerarun Steve Tubnonghee <trsteve@engr2-1-9-40-edu.int.colorado.edu>
2017-11-01 14:44:39 -0600

    add google analytics, fix subscription box

--- client/src/components/EmailOptIn/EmailOptIn.js
+++ client/src/components/EmailOptIn/EmailOptIn.js
@@ -5,22 +5,22 @@ class EmailOptIn extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      firstName: '',
+      name: '',
       email: ''
     }
-    this.handleFirstName = this.handleFirstName.bind(this)
+    this.handleName = this.handleName.bind(this)
     this.handleEmail = this.handleEmail.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleClose = this.handleClose.bind(this)
   }
-  handleFirstName(event) {
-    this.setState({firstName: event.target.value});
+  handleName(event) {
+    this.setState({name: event.target.value});
   }
   handleEmail(event) {
     this.setState({email: event.target.value});
   }
   handleSubmit(event) {
-    console.log('A name was submitted: ' + this.state.email + this.state.firstName + this.state.lastName)
+    console.log('A name was submitted: ' + this.state.email + this.state.name)
     this.addMember()
     event.preventDefault()
     window.location.href = 'https://www.facebook.com/'
@@ -37,7 +37,7 @@ class EmailOptIn extends Component {
         email_address: this.state.email,
         status: 'subscribed',
         merge_fields: {
-          firstName: this.state.firstName
+          name: this.state.name
         }
       })
     })
@@ -56,11 +56,11 @@ class EmailOptIn extends Component {
       <div className="email-opt-in" onClick={this.props.closeEmailOptIn}>
         <div className="registration-block" onClick={this.handleClose}>
           <h2>Join us now.</h2>
-          <h4>Sign up to grab limited exclusive access to Resonance.</h4>
+          <h4>Subscribe to grab limited exclusive access to Resonance.</h4>
           <form onSubmit={this.handleSubmit}>
-            <input placeholder="first name" type="text" value={this.state.firstName} onChange={this.handleFirstName} />
+            <input placeholder="John Resonate" type="text" value={this.state.name} onChange={this.handleName} />
             <input placeholder="email" type="text" value={this.state.email} onChange={this.handleEmail} />
-            <input type="submit" value="Get VIP access!" />
+            <input type="submit" value="Get Exclusive Access!" />
           </form>
         </div>
       </div>
